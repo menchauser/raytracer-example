a695bf672c1e3db48717e4e625cb7647c5534298
Mukhamed Karanashev <foobar@yandex-team.ru>
2018-03-10 00:40:53 +0300

    Generate simple gradient image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    let nx = 200;
+    let ny = 100;
+
+    print!("P3\n{} {}\n255\n", nx, ny);
+
+    for j in (0..ny).rev() {
+        for i in 0..nx {
+            let r = (i as f32) / (nx as f32);
+            let g = (j as f32) / (ny as f32);
+            let b = 0.2;
+
+            let ir = (255.99 * r) as u32;
+            let ig = (255.9 * g) as u32;
+            let ib = (255.9 * b) as u32;
+
+            print!("{} {} {}\n", ir, ig, ib);
+        }
+    }
 }
